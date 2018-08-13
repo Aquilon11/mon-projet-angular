6f33a0621c874738c59b3f83159bf43a0836b3f9
aquilon11 <aquilon11@gmail.com>
2018-08-14 00:05:15 +0200

    Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { interval } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  secondes: number
+
+  ngOnInit() {
+    interval(1000).subscribe(
+      value => {
+        this.secondes = value
+      },
+      error => {
+        console.log('Uh-oh, an error occurred! : ' + error)
+      },
+      () => {
+        console.log('Observable complete!')
+      }
+    )
+  }
 }
