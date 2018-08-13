45439fb44426b808ef3e9d34d46f441a9c000fd6
aquilon11 <aquilon11@gmail.com>
2018-08-13 19:21:31 +0200

    Gérez des données dynamiques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth = false
+
+  appareilOne = 'Machine à laver'
+  appareilTwo = 'Frigo'
+  appareilThree = 'Ordinateur'
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true
+    }, 4000)
+  }
+
+  onAllumer() {
+    console.log('On allume tout !')
+  }
 }
